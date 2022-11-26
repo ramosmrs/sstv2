7d81d5425bcb9d7455ba4b4ee69a7d60636cae73
Marcio <marcio.rsouza@gmail.com>
2022-11-26 11:58:45 -0300

    [Backend] Carga Inicial

---
+++ sstv2_be/src/main/java/com/example/sstv2/sstv2_be/Sstv2BeApplication.java
@@ -0,0 +1,13 @@
+package com.example.sstv2.sstv2_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sstv2BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sstv2BeApplication.class, args);
+	}
+
+}
---
+++ sstv2_be/src/test/java/com/example/sstv2/sstv2_be/Sstv2BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sstv2.sstv2_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sstv2BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
